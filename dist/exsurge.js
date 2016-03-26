b1b3830348aeda6fe5eca1545ff14bc92f411120
Matthew Spencer, OSJ <mspencer@osjusa.org>
2016-03-26 10:49:03 -0700

    Updated dist build files

--- dist/exsurge.js
+++ dist/exsurge.js
@@ -813,7 +813,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    // some words that are simply exceptions to standard syllabification rules!
 	    var wordExceptions = new Object();
 	
-	    // ui combos pronounced as dipthongs
+	    // ui combos pronounced as diphthongs
 	    wordExceptions["huius"] = ["hui", "us"];
 	    wordExceptions["cuius"] = ["cui", "us"];
 	    wordExceptions["huic"] = ["huic"];
@@ -1020,7 +1020,14 @@ return /******/ (function(modules) { // webpackBootstrap
 	      for (i = 0, end = this.vowels.length; i < end; i++) {
 	        index = workingString.indexOf(this.vowels[i], startIndex);
 	
-	        if (index >= 0) return { found: true, startIndex: index, length: 1 };
+	        if (index >= 0) {
+	          // if the first vowel found is a U, and it is immediately followed by another vowel, (e.g., sanguis, quis), the first u counts as a consonant:
+	          // (in practice, this only affects words such as equus that contain a uu, since the alphabetically earlier vowel would be found before the U)
+	          if (this.vowels[i] === 'u' && this.isVowel(workingString[index + 1])) {
+	            ++index;
+	          }
+	          return { found: true, startIndex: index, length: 1 };
+	        }
 	      }
 	
 	      // no vowels sets found after startIndex!
@@ -1050,9 +1057,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	    _this2.strongVowels = ['a', 'e', 'o', 'á', 'é', 'í', 'ó', 'ú'];
 	
-	    _this2.dipthongs = ["ai", "ei", "oi", "ui", "ia", "ie", "io", "iu", "au", "eu", "ou", "ua", "ue", "uo", "ái", "éi", "ói", "úi", "iá", "ié", "ió", "iú", "áu", "éu", "óu", "uá", "ué", "uó", "üe", "üi"];
+	    _this2.diphthongs = ["ai", "ei", "oi", "ui", "ia", "ie", "io", "iu", "au", "eu", "ou", "ua", "ue", "uo", "ái", "éi", "ói", "úi", "iá", "ié", "ió", "iú", "áu", "éu", "óu", "uá", "ué", "uó", "üe", "üi"];
 	
-	    _this2.uDipthongExpections = ["gue", "gui", "qua", "que", "qui", "quo"];
+	    _this2.uDiphthongExceptions = ["gue", "gui", "qua", "que", "qui", "quo"];
 	    return _this2;
 	  }
 	
@@ -1266,8 +1273,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	          if (d[0] === 'u' && index > 0) {
 	            var tripthong = s.substr(index - 1, 3).toLowerCase();
 	
-	            for (j = 0, endj = this.uDipthongExpections.length; i < endj; j++) {
-	              if (tripthong === this.uDipthongExpections[j]) {
+	            for (j = 0, endj = this.uDiphthongExceptions.length; i < endj; j++) {
+	              if (tripthong === this.uDiphthongExceptions[j]) {
 	                // search from after the u...
 	                return this.findVowelSegment(s, index + 1);
 	              }
