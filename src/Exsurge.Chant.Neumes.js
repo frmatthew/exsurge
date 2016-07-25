2f77e0adcd0086f66774fa44e4d6478a8be393e1
Benjamin Bloomfield <benjamin.bloomfield@travelgpa.com>
2016-07-25 11:45:38 -0400

    fixed single mora after clivis fixed morae after torculus

--- src/Exsurge.Chant.Neumes.js
+++ src/Exsurge.Chant.Neumes.js
@@ -569,7 +569,7 @@ export class Clivis extends Neume {
     //    under certain circumstances
     if (this.notes[1].morae.length) {
       var morae = this.notes[1].morae;
-      morae[0].horizontalOffset += this.notes[1].bounds.right() - this.notes[0].bounds.right();
+      if(morae.length > 1) morae[0].horizontalOffset += this.notes[1].bounds.right() - this.notes[0].bounds.right();
       if(this.notes[0].staffPosition - this.notes[1].staffPosition === 1 &&
           Math.abs(this.notes[1].staffPosition % 2) === 1) {
         morae.slice(-1)[0].positionHint = MarkingPositionHint.Below;
@@ -1183,7 +1183,17 @@ export class Torculus extends Neume {
     }
 
     // 1. episema on lower notes should be below, upper note above
-    // 2. morae: fixme: implement
+    // 2. like the podatus, mora on lower note needs to below
+    //    under certain circumstances
+    if (this.notes[2].morae.length) {
+      var morae = this.notes[2].morae;
+      if(morae.length > 1) morae[0].horizontalOffset += this.notes[2].bounds.right() - this.notes[1].bounds.right();
+      if(this.notes[1].staffPosition - this.notes[2].staffPosition === 1 &&
+          Math.abs(this.notes[2].staffPosition % 2) === 1) {
+        morae.slice(-1)[0].positionHint = MarkingPositionHint.Below;
+      }
+    }
+
     for (i = 0; i < this.notes[0].epismata.length; i++) {
       marking = this.notes[0].epismata[i];
 
